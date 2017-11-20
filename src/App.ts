7a15eec592afd982a75e71920e041d731c5c5c75
sahil.khanna <email.sahilkhanna@kochartech.com>
2017-11-20 21:17:38 +0530

    MongoDB
    1) Integrated MongoDB to the application

--- src/App.ts
+++ src/App.ts
@@ -2,20 +2,36 @@ import * as path from 'path';
 import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
+import * as mongodb from 'mongodb';
+import {MongoClient, Db} from 'mongodb';
+import {UserController} from './controllers/UserController';
 
-import {UserController} from './Controllers/UserController';
 
 // Creates and configures an ExpressJS web server.
 class App {
 
   // ref to Express instance
   public express: express.Application;
-
+  public db: Db;
+  
   //Run configuration methods on the Express instance.
   constructor() {
     this.express = express();
+    this.initDB();
     this.middleware();
     this.routes();
+  }
+
+  //TODO: Init route and middleware when DB finishes initializing
+  private initDB() {
+    let mongoClient = new MongoClient().connect('mongodb://sahil.khanna:MyPassword@ds113736.mlab.com:13736/user-profile')
+    .then((_db) => {
+      console.log(_db);
+      this.db = _db;
+    })
+    .catch((_err) => {
+      console.log(_err);
+    })
   }
 
   // Configure Express middleware.
@@ -30,25 +46,16 @@ class App {
     /* This is just to get up and running, and to make sure what we've got is
      * working so far. This function will change when we start to add more
      * API endpoints */
+
     let router = express.Router();
+    let urlPrefix: String = '/api/1.0/';
+    let app = this.express;
     // placeholder route handler
-    // router.get('/', (req, res, next) => {
-    //   res.json({
-    //     message: 'Hello World!'
-    //   });
-    // });
-    
-    // this.express.use(UserController.route, new UserController().router);
 
-    router.get('/api/1.0/*', (req, res) => {
-      console.log(req.url);
-      res.json({
-        code: 0,
-        message: 'Hello World'
-      });
-    });
+    console.log(this.db);
 
-    this.express.use('/', router);
+    app.post(urlPrefix + 'login', new UserController(this.db).login);
+    app.post(urlPrefix + 'register', new UserController(this.db).register);
   }
 
 }
--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,11 +1,31 @@
 import {Router, Request, Response} from 'express';
+import {Db} from 'mongodb';
 
 export class UserController {
 
-    public static route: string = 'login';
-    public router: Router = Router();
+    private db: Db;
 
-    public sayHello(req: Request, res: Response) {
-        res.send('Hello-World');
+    constructor(_db: Db) {
+        this.db = _db;
+        console.log(this.db);
+    }
+
+    public login(req: Request, res: Response) {
+        res.json({
+            code: 0,
+            message: 'Login',
+            data: req.body
+        })
+    }
+
+    public register(req: Request, res: Response) {
+        this.db.collection('user').insert({
+            email: req.body.email
+        })
+        res.json({
+            code: 0,
+            message: 'Register',
+            data: null
+        });
     }
 }
